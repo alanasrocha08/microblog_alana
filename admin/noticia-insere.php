91c9204bc765be3f8491f1af755269110aa2bf59
alanasrocha08 <alanasr02@gmail.com>
2025-05-21 10:53:26 -0300

    Atualiza

--- admin/noticia-insere.php
+++ admin/noticia-insere.php
@@ -3,22 +3,39 @@ require_once "../vendor/autoload.php";
 
 use Microblog\Auth\ControleDeAcesso;
 use Microblog\Enums\Destaque;
+use Microblog\Enums\TipoUsuario;
 use Microblog\Helpers\Utils;
 use Microblog\Helpers\Validacoes;
 use Microblog\Models\Noticia;
 use Microblog\Services\CategoriaServico;
 use Microblog\Services\NoticiaServico;
 
-//Acessando os dados da sessão referente ao usuário logado
+$idNoticia = Utils::sanitizar($_GET["id"], "inteiro");
+Utils::verificarId($idNoticia);
+
+// Configurar após programar Controle de Acesso
 ControleDeAcesso::exigirLogin();
 $idUsuario = $_SESSION['id'];
+$tipoUsuario = TipoUsuario::from($_SESSION['tipo']);
+
 
 $mensagemErro = "";
 
 $categoriaServico = new CategoriaServico();
 $listaDeCategorias = $categoriaServico->listarTodos();
 
-if (isset($_POST["inserir"])) {
+$noticiaServico = new NoticiaServico();
+$dados = $noticiaServico->buscarPorId($idNoticia, $tipoUsuario, $idUsuario);
+
+
+if (empty($dados)) {
+    Utils::alertaErro("Notícia não encontrada.");
+    header("location:noticias.php");
+    exit;
+}
+
+
+if (isset($_POST["atualizar"])) {
     $titulo = Utils::sanitizar($_POST["titulo"]);
     $texto = Utils::sanitizar($_POST["texto"]);
     $resumo = Utils::sanitizar($_POST["resumo"]);
@@ -28,6 +45,7 @@ if (isset($_POST["inserir"])) {
     // Captura o arquivo de imagem
     $arquivoDeImagem = Utils::sanitizar($_FILES["imagem"], "arquivo");
 
+
     try {
         Validacoes::validarCategoria($categoria);
         Validacoes::validarTitulo($titulo);
@@ -35,17 +53,20 @@ if (isset($_POST["inserir"])) {
         Validacoes::validarResumo($resumo);
         Validacoes::validarDestaque($destaque);
 
-        // Envia o arquivo de imagem para o servidor
-        Utils::upload($arquivoDeImagem);
-
-        // Captura o nome do arquivo
-        $nomeDaImagem = $arquivoDeImagem["name"];
         $destaqueEnum = Destaque::from($destaque);
 
-        $noticia = new Noticia($titulo, $texto, $resumo, $nomeDaImagem, $destaqueEnum, $idUsuario, $categoria);
+        // Se enviou nova imagem, faz upload e substitui
+        if ($arquivoDeImagem !== null) {
+            Utils::upload($arquivoDeImagem);
+            $nomeDaImagem = $arquivoDeImagem["name"]; // captura o nome do arquivo
+        } else {
+            $nomeDaImagem = $dados['imagem']; // Mantém a imagem antiga
+        }
+
+        $noticia = new Noticia($titulo, $texto, $resumo, $nomeDaImagem, $destaqueEnum, $idUsuario, $categoria, $idNoticia);
 
         $noticiaServico = new NoticiaServico();
-        $noticiaServico->inserir($noticia);
+        $noticiaServico->atualizar($noticia, $tipoUsuario);
 
         header("location:noticias.php");
         exit;
@@ -55,7 +76,6 @@ if (isset($_POST["inserir"])) {
     }
 }
 
-
 require_once "../includes/cabecalho-admin.php";
 ?>
 
@@ -64,7 +84,7 @@ require_once "../includes/cabecalho-admin.php";
     <article class="col-12 bg-white rounded shadow my-1 py-4">
 
         <h2 class="text-center">
-            Inserir nova notícia
+            Atualizar dados da notícia
         </h2>
 
         <?php if (!empty($mensagemErro)) : ?>
@@ -73,8 +93,7 @@ require_once "../includes/cabecalho-admin.php";
             </div>
         <?php endif; ?>
 
-        <!-- Para que o formulário aceite arquivos (upload), é necessário habilitar o atributo enctype -->
-        <form class="mx-auto w-75" action="" method="post" id="form-inserir" name="form-inserir" enctype="multipart/form-data">
+        <form class="mx-auto w-75" action="" method="post" id="form-atualizar" name="form-atualizar" enctype="multipart/form-data">
 
             <div class="mb-3">
                 <label class="form-label" for="categoria">Categoria:</label>
@@ -82,7 +101,9 @@ require_once "../includes/cabecalho-admin.php";
                     <option value=""></option>
 
                     <?php foreach ($listaDeCategorias as $itemCategoria) { ?>
-                        <option value="<?= $itemCategoria['id'] ?>">
+                        <option
+                            <?php if ($dados["categoria_id"] === $itemCategoria["id"]) echo " selected " ?>
+                            value="<?= $itemCategoria['id'] ?>">
                             <?= $itemCategoria['nome'] ?>
                         </option>
                     <?php } ?>
@@ -92,38 +113,54 @@ require_once "../includes/cabecalho-admin.php";
 
             <div class="mb-3">
                 <label class="form-label" for="titulo">Título:</label>
-                <input class="form-control" type="text" id="titulo" name="titulo">
+                <input value="<?= $dados['titulo'] ?>" class="form-control" type="text" id="titulo" name="titulo">
             </div>
 
             <div class="mb-3">
                 <label class="form-label" for="texto">Texto:</label>
-                <textarea class="form-control" name="texto" id="texto" cols="50" rows="6"></textarea>
+                <textarea class="form-control" name="texto" id="texto" cols="50" rows="6"><?= $dados['texto'] ?></textarea>
             </div>
 
             <div class="mb-3">
                 <label class="form-label" for="resumo">Resumo (máximo de 300 caracteres):</label>
                 <span id="maximo" class="badge bg-danger">0</span>
-                <textarea class="form-control" name="resumo" id="resumo" cols="50" rows="2" maxlength="300"></textarea>
+                <textarea class="form-control" name="resumo" id="resumo" cols="50" rows="2" maxlength="300"><?= $dados['resumo'] ?></textarea>
             </div>
 
             <div class="mb-3">
-                <label class="form-label" for="imagem" class="form-label">Selecione uma imagem:</label>
-                <input class="form-control" type="file" id="imagem" name="imagem"
-                    accept="image/png, image/jpeg, image/gif, image/svg+xml">
+                <div class="row">
+                    <div class="col-5">
+                        <label for="imagem-existente" class="form-label">Imagem atual:</label>
+                        <!-- campo somente leitura, meramente informativo -->
+                        <input value="<?= $dados['imagem'] ?>" class="form-control" type="text" id="imagem-existente" name="imagem-existente" readonly disabled>
+                    </div>
+                    <div class="col-7 text-end">
+                        <img src="../images/<?= $dados['imagem'] ?>" alt="Imagem da notícia" class="img-fluid rounded">
+                    </div>
+                </div>
+            </div>
+
+            <div class="mb-3">
+                <label for="imagem" class="form-label">Caso queira mudar, selecione outra imagem:</label>
+                <input class="form-control" type="file" id="imagem" name="imagem" accept="image/png, image/jpeg, image/gif, image/svg+xml">
             </div>
 
             <div class="mb-3">
                 <p>Deixar a notícia em destaque?
-                    <input type="radio" class="btn-check" name="destaque" id="option1" checked autocomplete="off" value="nao">
+                    <!-- aparentemente, o id precisa ser optionX -->
+                    <input
+                        <?php if ($dados["destaque"] === "nao") echo " checked " ?>
+                        type="radio" class="btn-check" name="destaque" id="option1" autocomplete="off" checked value="nao">
                     <label class="btn btn-outline-danger" for="option1">Não</label>
 
-                    <input type="radio" class="btn-check" name="destaque" id="option2" autocomplete="off" value="sim">
+                    <input
+                        <?php if ($dados["destaque"] === "sim") echo " checked " ?>
+                        type="radio" class="btn-check" name="destaque" id="option2" autocomplete="off" value="sim">
                     <label class="btn btn-outline-success" for="option2">Sim</label>
                 </p>
-
             </div>
 
-            <button class="btn btn-primary" id="inserir" name="inserir"><i class="bi bi-save"></i> Inserir</button>
+            <button class="btn btn-primary" name="atualizar"><i class="bi bi-arrow-clockwise"></i> Atualizar</button>
         </form>
 
     </article>
